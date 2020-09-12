b1051b18ee5fdbf7e1cb4f7563e9db8ebc006138
Surachet Yaitammasan <aottapkub@hotmail.com>
2020-09-12 12:22:25 +0700

    Initial Commit

---
+++ JordYet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JordYet
+//
+//  Created by Surachat Yaitammasan on 12/9/2563 BE.
+//  Copyright © 2563 Surachat Yaitammasan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
