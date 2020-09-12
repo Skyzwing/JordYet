9d95fe22a169ee5e5986eb5e0fb7b0815d0f6c9a
Surachet Yaitammasan <aottapkub@hotmail.com>
2020-09-12 12:38:07 +0700

    custom map

---
+++ JordYet/Map/LandmarkMode.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkMode.swift
+//  JordYet
+//
+//  Created by Surachat Yaitammasan on 12/9/2563 BE.
+//  Copyright © 2563 Surachat Yaitammasan. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class LandmarkModel: NSObject, MKAnnotation {
+    let title: String?
+    let subtitle: String?
+    let coordinate: CLLocationCoordinate2D
+
+    init(title: String?,
+         subtitle: String?,
+         coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+    
+    static func requestMockData()-> [LandmarkModel]{
+        return [
+            LandmarkModel(title: "Bengalore",
+                               subtitle:"Bengaluru (also called Bangalore) is the capital of India's southern Karnataka state.",
+                               coordinate: .init(latitude: 12.9716, longitude: 77.5946)),
+            LandmarkModel(title: "Mumbai",
+                               subtitle:"Mumbai (formerly called Bombay) is a densely populated city on India’s west coast",
+                               coordinate: .init(latitude: 19.0760, longitude: 72.8777))
+        ]
+    }
+}
